d2ea00fb3a6ce54f66224f9bbfa4df7cf390d83c
Aivenho <nauris.sedlers@gmail.com>
2016-06-15 11:22:39 +0300

    Update __openerp__.py

--- poseidon/__openerp__.py
+++ poseidon/__openerp__.py
@@ -1,26 +1,16 @@
 # -*- coding: utf-8 -*-
 
-
 {
     'name': 'Poseidon',
-
     'description': 'Temperature Import Module',
-
     'category': 'Extra Tools',
-
     'author': 'Aivenhoo, Faycal',
-
     'depends': [
-                
                 ],
-
     'data': [
-             #Classic Odoo form extensions + New forms
             'temperature.xml',
             "security/temperature_user.xml",
             "security/ir.model.access.csv",
              ],
-
     'application': 'True'
-
 }
