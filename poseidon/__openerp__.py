91e824363a6499115ef28a3329b0bfaa8b989fd5
Aivenho <nauris.sedlers@gmail.com>
2016-06-15 12:39:46 +0300

    Update __openerp__.py

--- poseidon/__openerp__.py
+++ poseidon/__openerp__.py
@@ -1,10 +1,37 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015  Interlux, SIA  (http://www.interlux.lv)
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
     'name': 'Poseidon',
+    'version': '8.0.0.0.0',
     'description': 'Temperature Import Module',
-    'category': 'Extra Tools',
+    'license': 'AGPL-3',
+    'category': 'Tools',
     'author': 'Aivenhoo, Faycal',
+        'description': """
+Poseidon 2250 (HW group) data import
+=================
+Solution to integrate temperature
+documentation in to Odoo database.
+""",
     'depends': [
                 ],
     'data': [
