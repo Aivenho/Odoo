a7de056e8729ac26d4306d45112e93a7d94eb59a
Aivenho <nauris.sedlers@gmail.com>
2017-02-21 11:18:31 +0200

    Update tempread.py

--- poseidon/models/tempread.py
+++ poseidon/models/tempread.py
@@ -13,16 +13,16 @@ import datetime,parser
 class temperature(models.Model):
     _name= 'temperature'
     
-    ip=fields.Char(string="Ip",required=True)
-    time_intervale=fields.Integer(string="Time intervale")
+    ip=fields.Char(string="IP Address",required=True)
+    time_intervale=fields.Integer(string="Time Interval")
     data=fields.One2many('temperatures','datas')
     last_update = fields.Datetime('Last update')
     
-    def get_temperature(self, cr, uid, context=None):
+    def get_temperature(self,cr,uid,context=None):
         temp_obj = self.pool.get('temperatures')
 
-        ids = self.search(cr, uid,[], context=context)
-        for rec in self.browse(cr, uid, ids, context=context):
+        ids = self.search(cr,uid,[],context=context)
+        for rec in self.browse(cr,uid,ids,context=context):
             a = rec.ip
             uu = urllib2.urlopen(a)
 
@@ -37,7 +37,7 @@ class temperature(models.Model):
                 min = temp.find('Min').text
                 max = temp.find('Max').text
                 newdate = fields.Datetime.now().rpartition(':')[0]
-                date_time = datetime.datetime.strptime(newdate, "%Y-%m-%d %H:%M")
+                date_time = datetime.datetime.strptime(newdate,"%Y-%m-%d %H:%M")
                 dat_time = date_time.strftime("%Y-%m-%d %H:%M")
                 dt_time = str(dat_time).replace('-','.')
 
@@ -53,7 +53,7 @@ class temperature(models.Model):
                             'dt_time': dt_time,
                 }
                 flag = temp_obj.create(cr,uid,vals_temp,context=context)
-            self.write(cr, uid,ids,{'last_update':vals_temp['datos']})
+            self.write(cr,uid,ids,{'last_update':vals_temp['datos']})
 
 class temperatures(models.Model):
     _name = 'temperatures'
@@ -64,6 +64,6 @@ class temperatures(models.Model):
     value=fields.Float(string="Actual Value")
     min=fields.Float(string="Min")
     max=fields.Float(string="Max")
-    datas=fields.Many2one('temperature',readonly=True)
-    datos = fields.Datetime('Date', required=True,readonly=True)
-    dt_time = fields.Char('Date & Time')
+    datas=fields.Many2one('Temperature',readonly=True)
+    datos=fields.Datetime('Date', required=True,readonly=True)
+    dt_time=fields.Char('Date & Time')
